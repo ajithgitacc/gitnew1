bd18ce1c678bb99dab380a5e1fdec9cb3230441a
ajithgitacc <ajithkumarmoorthy11@gmail.com>
2024-04-15 17:59:23 +0530

    finalcodedinesh

--- src/test/java/org/git1503/Task.java
+++ src/test/java/org/git1503/Task.java
@@ -4,6 +4,9 @@ public class Task {
 	private void day1() {
 		System.out.println("employee details");
 	}
+	private void day3() {
+		System.out.println("client details");
+	}
 	public static void main(String[] args) {
 		Task task= new Task();
 		task.day1();

837b744fd603f92223931c95f7cf434db21561a4
ajithgitacc <ajithkumarmoorthy11@gmail.com>
2024-04-15 14:14:59 +0530

    my task1

---
+++ src/test/java/org/git1503/Task.java
@@ -0,0 +1,12 @@
+package org.git1503;
+
+public class Task {
+	private void day1() {
+		System.out.println("employee details");
+	}
+	public static void main(String[] args) {
+		Task task= new Task();
+		task.day1();
+	}
+
+}
